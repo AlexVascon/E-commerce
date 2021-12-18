fb42083c34322c84a8630f00a6453bfc8a5ec6d7
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-18 21:37:09 +0100

    feat: add login logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from '../models/userModel.js'
+import bcrypt from 'bcrypt'
+import {generateAccessToken} from '../utils/generateToken.js'
 
 export const register = async (req,res,next) => {
   try {
@@ -11,6 +13,23 @@ export const register = async (req,res,next) => {
     })
     await user.save()
     res.status(200).send(user)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const login = async (req,res,next) => {
+  try {
+    const {usernameOrEmail, password} = req.body
+    const searchCriteria = await usernameOrEmail.includes('@') ? 'email' : 'username'
+    const user = await User.findOne( { [searchCriteria]: usernameOrEmail} ).lean().orFail()
+    if(!bcrypt.compareSync(password, user.password)) return res.status(400).send({message: 'Invalid username or password'})
+    const payload = {
+      username: user.username,
+      _id: user._id,
+    }
+    const accessToken = generateAccessToken(payload)
+    res.status(200).send(accessToken)
   } catch (err) {
     next(err)
   }

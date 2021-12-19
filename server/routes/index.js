781aa326ddf2c4fc91c84612ad3b24e8f992fe05
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-19 18:36:07 +0100

    add cart route to all routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import userRoutes from './userRoutes.js'
 import productRoutes from './productRoutes.js'
+import cartRoutes from './cartRoutes.js'
 
 export default (app) => {
   app.use('/users', userRoutes)
   app.use('/products', productRoutes)
+  app.use('/cart', cartRoutes)
 }
 

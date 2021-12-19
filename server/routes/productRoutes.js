4f27c685e5292409b39f74bb8a7cfa830d1d75de
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-19 03:14:21 +0100

    fix: add auth token to admin routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import {category, createProduct, deleteProduct, productDetails, suggestions} from '../controllers/productController.js'
-import {admin} from '../middleware/authMiddleware.js'
+import {category, createProduct, deleteProduct, productDetails, suggestions, allProducts} from '../controllers/productController.js'
+import {authenticateToken, admin} from '../middleware/authMiddleware.js'
 
 router.get('/category', category)
 router.get('/delete', admin, deleteProduct)
 router.post('/create', admin, createProduct)
 router.get('/:itemId', productDetails)
 router.get('/suggestions/:selection/:category/:itemId', suggestions)
+router.get('/all', authenticateToken, admin, allProducts)
 
 export default router

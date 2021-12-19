5ccee7dc8be27a26684f5208051c24a5e4adba52
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-19 01:55:49 +0100

    add product routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 import userRoutes from './userRoutes.js'
+import productRoutes from './productRoutes.js'
 
 export default (app) => {
   app.use('/users', userRoutes)
+  app.use('/products', productRoutes)
 }
 

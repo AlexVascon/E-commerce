937785174d25a53c5f564b4097da60ba2f59b631
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-19 01:41:52 +0100

    feat: add product controller and category logic

---
+++ server/controllers/productController.js
@@ -0,0 +1,31 @@
+import Product from '../models/productModel.js'
+
+// example request: url/category?selection=women&category=shirt&page=1&limit=7
+export const category = async (req,res, next) => {
+  try {
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit)
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const results = {}
+
+    const allProducts = await Product.find({selection: req.query.selection, category: req.query.category}).lean()
+    if(endIndex < allProducts.length) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      }
+    }
+
+    if(startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      }
+    }
+    const categoryItems = await Product.find({selection: req.query.selection, category: req.query.category}).limit(limit).skip(startIndex).exec()
+    res.status(200).send({items: categoryItems, count: allProducts.length, page: page })
+  } catch (err) {
+    next(err)
+  }
+}

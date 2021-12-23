a07630763feffd52aba31c63e9f800dd2453d844
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 13:01:06 +0100

    feat: add selection logic and link to product details

--- client/src/views/Product.js
+++ client/src/views/Product.js
@@ -1,35 +1,81 @@
-import React from 'react'
-import { ViewResponsive, Section, List, Row, RowText } from '../components/View'
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { fetchProductInformation } from '../actions/productActions'
+import { ViewResponsive, Section, List, Row, RowText, RowResponsive } from '../components/View'
 import {Button} from '../components/Button'
 import productImg from '../assets/fade_item_background.jpg'
 import styled from 'styled-components'
+import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined'
+import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
 
 export default function Product() {
+  const {productId} = useParams()
+  const dispatch = useDispatch()
+  const {foundProductInformation} = useSelector((state) => state.fetchProductInformation)
+  const [openDescription, setOpenDescription] = useState(false)
+  const [openReviews, setOpenReviews] = useState(false)
+
+  useEffect(() => {
+    dispatch(fetchProductInformation(productId))
+  }, [dispatch, productId])
+
+
+
   return (
     <ViewResponsive>
       <Section imageUrl={process.env.PUBLIC_URL + productImg}>
       <Display>
-      <Title>Title</Title>
-      <Price>$40</Price>
+      <Title>{foundProductInformation && foundProductInformation.title}</Title>
+      <Price>${foundProductInformation && foundProductInformation.price}</Price>
       <ImageContainer>
-        <Image />
+        <Image src={foundProductInformation && foundProductInformation.image} />
       </ImageContainer>
       </Display>
       </Section>
       <Section>
         <List>
+        <RowResponsive onClick={() => setOpenReviews(!openReviews)}>
           <Row>
           <RowText>Review</RowText>
-          <RowText>Show</RowText>
+          {
+            !openReviews 
+            ?
+            <ExpandCircleDownOutlinedIcon fontSize='large' />
+            :
+            <ExpandLessOutlinedIcon fontSize='large' />
+          }
           </Row>
+          {openReviews 
+          &&
+          <ReviewsContainer>
+
+          </ReviewsContainer>
+          }
+          
+        </RowResponsive>
           <Row>
           <RowText>Stock</RowText>
-          <RowText>In Stock</RowText>
+          <RowText>{foundProductInformation?.quantity > 0 ? 'In stock' : 'Out of stock'}</RowText>
           </Row>
-          <Row>
+          <RowResponsive>
+          <Row onClick={() => setOpenDescription(!openDescription)}>
           <RowText>Description</RowText>
-          <RowText>Show</RowText>
+          {
+            !openDescription 
+            ?
+            <ExpandCircleDownOutlinedIcon fontSize='large' />
+            :
+            <ExpandLessOutlinedIcon fontSize='large' />
+          }
           </Row>
+          {openDescription 
+          &&
+          <DescriptionContainer>
+            <Description>{foundProductInformation && foundProductInformation.description}</Description>
+          </DescriptionContainer>
+          }
+          </RowResponsive>
         </List>
         <Button light={true}>ADD TO CART</Button>
       </Section>
@@ -51,6 +97,7 @@ position: absolute;
   color: rgb(214, 175, 1);
   font-size: 4rem;
   font-weight: 700;
+  z-index: 3;
 `
 const Price = styled.p`
 position: absolute;
@@ -63,12 +110,31 @@ padding: .5rem .8rem;
   color: white;
 `
 
-const Image = styled.img``
+const Image = styled.img`
+`
 const ImageContainer = styled.div`
 position: absolute;
   bottom: 15%;
   right: 0%;
-  z-index: 0;
+  z-index: 1;
+`
+
+const DescriptionContainer = styled.div`
+ display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+  height: 10rem;
+  width: 100%;
+`
+
+const Description = styled.p``
+
+const ReviewsContainer = styled.div`
+display: flex;
+  flex-direction: column;
+  height: 25rem;
+  width: 100%;
+  overflow-y: scroll;
 `
 
 

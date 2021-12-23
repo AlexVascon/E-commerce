db12459e2a4bfbaa0024a6abf99ae50e813d0ec9
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 13:00:10 +0100

    feat: add register page logic

--- client/src/views/Portal.js
+++ client/src/views/Portal.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import { View } from '../components/View'
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { register } from '../actions/userActions'
+import { useNavigate } from 'react-router-dom'
+import { View, Error } from '../components/View'
 import portalImg from '../assets/greyscale_mountains.jpg'
 import { Form, Input } from '../components/Form'
 import { Button } from '../components/Button'
 import styled from 'styled-components'
 
 export default function Portal() {
+  const dispatch = useDispatch()
+  const { registeredUser, registerError} = useSelector((state) => state.register)
+  const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleUsernameChange = (e) => setUsername(e.target.value)
+  const handleEmailChange = (e) => setEmail(e.target.value)
+  const handlePasswordChange = (e) => setPassword(e.target.value)
+  const handleConfirmPasswordChange = (e) => setConfirmPassword(e.target.value)
+
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault()
+    dispatch(register(username, email, password, confirmPassword))
+  }
+
+  useEffect(() => {
+    if(registeredUser) navigate('/account')
+  }, [registeredUser, navigate])
+
   return (
     <View imageUrl={process.env.PUBLIC_URL + portalImg} >
-    <Form>
+    <Form onSubmit={handleRegisterSubmit}>
     <Options>
       <Btn>Login</Btn>
       /
       <Btn>Register</Btn>
     </Options>
-      <Input type='text' placeholder='Username' />
-      <Input type='email' placeholder='Email' />
-      <Input type='password' placeholder='Password' />
-      <Input type='password' placeholder='Confirm password' />
+    {registerError && <Error>{registerError}</Error>}
+      <Input type='text' placeholder='Username' onChange={handleUsernameChange} />
+      <Input type='email' placeholder='Email' onChange={handleEmailChange} />
+      <Input type='password' placeholder='Password' onChange={handlePasswordChange} />
+      <Input type='password' placeholder='Confirm password' onChange={handleConfirmPasswordChange} />
       <Button light={true} type='submit'>REGISTER</Button>
     </Form>
     </View>

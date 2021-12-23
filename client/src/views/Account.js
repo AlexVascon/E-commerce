e0aa547984f3d9408fc3dbd07bef2f556beddda6
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 14:01:10 +0100

    fix: add account links

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ViewResponsive } from '../components/View'
+import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import accountImg from '../assets/greyscale_mountains.jpg'
 
@@ -7,17 +8,21 @@ import accountImg from '../assets/greyscale_mountains.jpg'
 export default function Account() {
   return (
     <ViewResponsive imageUrl={process.env.PUBLIC_URL + accountImg}>
-      <Text>Shipping address</Text>
-      <Text>Cart</Text>
-      <Text>Logout</Text>
+      <Text to='/shipping'>Shipping address</Text>
+      <Text to='/cart'>Cart</Text>
+      <Text to='/logout'>Logout</Text>
     </ViewResponsive>
   )
 }
 
-const Text = styled.h2`
+const Text = styled(Link)`
 color: rgba(255, 235, 148, 0.945);
-font-size: 1.7rem;
+font-size: 2rem;
+font-weight: bold;
 text-align: center;
 align-self: center;
 justify-self: center;
+text-decoration: none;
+align-self: center;
+margin: auto;
 `

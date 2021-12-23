aa8af0890ece34938ff5791435e27333ea01fa62
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 19:40:06 +0100

    feat: add login and toggle form

--- client/src/views/Portal.js
+++ client/src/views/Portal.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { register } from '../actions/userActions'
+import { register, login } from '../actions/userActions'
 import { useNavigate } from 'react-router-dom'
 import { View, Error } from '../components/View'
 import portalImg from '../assets/greyscale_mountains.jpg'
@@ -12,19 +12,27 @@ export default function Portal() {
   const dispatch = useDispatch()
   const { registeredUser, registerError} = useSelector((state) => state.register)
   const navigate = useNavigate()
+  const [isLogin, setIsLogin] = useState(true)
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [usernameOrEmail, setUsernameOrEmail] = useState('')
 
   const handleUsernameChange = (e) => setUsername(e.target.value)
   const handleEmailChange = (e) => setEmail(e.target.value)
   const handlePasswordChange = (e) => setPassword(e.target.value)
   const handleConfirmPasswordChange = (e) => setConfirmPassword(e.target.value)
+  const handleUsernameOrEmailChange = (e) => setUsernameOrEmail(e.target.value)
 
   const handleRegisterSubmit = async (e) => {
     e.preventDefault()
     dispatch(register(username, email, password, confirmPassword))
+  }
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault()
+    dispatch(login(usernameOrEmail, password))
   }
 
   useEffect(() => {
@@ -33,12 +41,19 @@ export default function Portal() {
 
   return (
     <View imageUrl={process.env.PUBLIC_URL + portalImg} >
-    <Form onSubmit={handleRegisterSubmit}>
-    <Options>
-      <Btn>Login</Btn>
+     <Options>
+      <Btn onClick={() => setIsLogin(true)}>Login</Btn>
       /
-      <Btn>Register</Btn>
+      <Btn onClick={() => setIsLogin(false)}>Register</Btn>
     </Options>
+    {isLogin ?
+      <Form onSubmit={handleLoginSubmit}>
+    <Input type='text' placeholder='Type username or email' onChange={handleUsernameOrEmailChange} />
+    <Input type='password' placeholder='Password' onChange={handlePasswordChange} />
+    <Button light={true} type='submit'>LOGIN</Button>
+    </Form>
+    :
+    <Form onSubmit={handleRegisterSubmit}>
     {registerError && <Error>{registerError}</Error>}
       <Input type='text' placeholder='Username' onChange={handleUsernameChange} />
       <Input type='email' placeholder='Email' onChange={handleEmailChange} />
@@ -46,6 +61,7 @@ export default function Portal() {
       <Input type='password' placeholder='Confirm password' onChange={handleConfirmPasswordChange} />
       <Button light={true} type='submit'>REGISTER</Button>
     </Form>
+    }
     </View>
   )
 }
@@ -57,11 +73,14 @@ background: none;
 color: white;
 font-weight: 600;
 `
-
 const Options = styled.div`
 background-color: none;
   display: flex;
   width: 80%;
-  padding: 1rem 0;
+  padding: 0;
   color: white;
+  justify-self: center;
+  align-self: center;
+  margin: auto;
+  margin-bottom: 0;
 `

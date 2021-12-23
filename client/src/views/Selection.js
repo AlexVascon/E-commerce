ea919335860981aa8fa69e5666222283fec44247
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 13:01:50 +0100

    feat: add selection logic

--- client/src/views/Selection.js
+++ client/src/views/Selection.js
@@ -1,32 +1,81 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams, Link} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../actions/productActions'
 import { Image } from '../components/Image'
 import {View, Section} from '../components/View'
 import MenImg from '../assets/ruthson-zimmerman-hDANeGXvWRw-unsplash.jpg'
 import WomenImg from '../assets/zeny-rosalina-Z3tc0Bfv0c4-unsplash.jpg'
 import styled from 'styled-components'
 import Card from '../components/Card'
+import ReactPaginate from 'react-paginate'
 
 
 export default function Selection() {
+  const {gender} = useParams()
+  const dispatch = useDispatch()
+  const {foundProducts} = useSelector((state) => state.fetchProducts)
+  const [category, setCategory] = useState('shirt') // default
+  const [pageNumber, setPageNumber] = useState(0)
+  const [restPagnation, setResetPagnation] = useState(true)
 
+  const requestCategory = (category) => {
+    setCategory(category)
+    setResetPagnation(true)
+    setPageNumber(pageNumber => pageNumber = 0)
+  }
+
+  useEffect(() => {
+    dispatch(fetchProducts(gender, category, pageNumber))
+  }, [dispatch, category, pageNumber, gender])
+
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected)
+  }
+
+  const pagnation = () => {
+    return (
+      <ReactPaginate
+      previousLabel={'Previous'}
+      nextLabel={'Next'}
+      pageCount={2}
+      onPageChange={changePage}
+      forcePage={pageNumber}
+      containerClassName={'pagination-btns'}
+      previousLinkClassName={'previous-btn'}
+      nextLinkClassName={'next-btn'}
+      disabledClassName={'pagination-disabled'}
+      activeClassName={'pagination-active'}
+    />
+    )
+  }
+  
   return (
     <View>
     <Section>
-      <Image imageUrl={process.env.PUBLIC_URL + WomenImg}>
+      <Image imageUrl={process.env.PUBLIC_URL + gender === 'women' ? WomenImg : MenImg}>
       <Categories>
-        <List>SHIRT</List>
-        <List>JUMPER</List>
-        <List>SUIT</List>
+        <List onClick={() => requestCategory('shirt')}>SHIRT</List>
+        <List onClick={() => requestCategory('jumper')}>JUMPER</List>
+        <List onClick={() => requestCategory(gender === 'women' ? 'dress' : 'suit')}>{gender === 'women' ? 'DRESS' : 'SUIT'}</List>
       </Categories>
       </Image>
     </Section>
     <Section>
     <SelectionItems>
-    <Card.Container>
-      <Card.Title>Title</Card.Title>
-      <Card.Price>$20</Card.Price>
+    {foundProducts && foundProducts.products.map(item => {
+      return (
+        
+      <Card.Container to={`/product/${item._id}`} key={item._id} >
+      <Card.Image src={item.image}  />
+      <Card.Title>{item.title}</Card.Title>
+      <Card.Price>${item.price}</Card.Price>
     </Card.Container>
+      )
+    })}
     </SelectionItems>
+    {category && pagnation()}
     </Section>
     </View>
   )

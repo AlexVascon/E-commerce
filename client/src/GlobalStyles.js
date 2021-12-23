26829dc99032313505de6ba57c8475b54a2c695d
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-23 12:48:39 +0100

    style: add pagination styling

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -10,4 +10,27 @@ body {
 html {
    height: -webkit - fill - available;
 }
+
+${'' /* easily style pagination component from external library */}
+.pagination-btns {
+  width: 100%;
+  height: 40px;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+}
+
+.pagination-btns a {
+  padding: 10px;
+  margin: 8px;
+  border-radius: 5px;
+  border: 1px solid grey;
+  color: grey;
+}
+
+.pagination-active a {
+  color: white;
+  background-color: grey;
+}
 `

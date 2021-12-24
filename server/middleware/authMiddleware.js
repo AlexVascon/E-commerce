116f01f1e2654507dad439371b0a512a96ef35dd
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-12-24 19:36:05 +0100

    fix: promise exception warning

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,25 +2,23 @@ import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js'
 
 export const authenticateToken = async (req, res, next) => {
-  let token
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1]
-      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-      req.user = await User.findById(decoded._id).select('-password')
-      next()
-    } catch (err) {
-      res.status(401)
-      throw new Error('Not authorized, token fail')
-    }
-  }
-
-  if (!token) {
-    res.status(401)
+  try {
+    let token
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        req.user = await User.findById(decoded._id).select('-password')
+        next()
+  } else {
+    return res.status(401).send({messages: 'no token'})
     throw new Error("Not authorized, no token")
+  }
+  } catch (err) {
+    res.status(401).send({messages: 'invalid token'})
+      throw new Error('Not authorized, token fail')
   }
 }
 

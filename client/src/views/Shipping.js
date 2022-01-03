8b444efe5489fff6ecba5c41e4ab82e060272656
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-03 22:47:13 +0100

    edit error and loading for shipping page

--- client/src/views/Shipping.js
+++ client/src/views/Shipping.js
@@ -1,14 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { saveShippingInformation, fetchShippingInformation } from '../actions/userActions'
-import { View, Button, Error, LoadingSpinner, Heading, Message } from '../components/MyLibrary'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  saveShippingInformation,
+  fetchShippingInformation,
+} from '../actions/userActions'
+import {
+  View,
+  Button,
+  Error,
+  LoadingSpinner,
+  Heading,
+  Message,
+} from '../components/MyLibrary'
 import shippingImg from '../assets/cloudy_mountain_DARK.jpg'
 import { Form, FormGroup, Input } from '../components/Form'
 
 export default function Shipping() {
   const dispatch = useDispatch()
-  const {saveShippingInformationError, loadingSaveShipping, saveShippingInformationSuccess} = useSelector((state) => state.saveShippingInformation)
-  const {shippingInformation, fetchShippingInformationError} = useSelector((state) => state.fetchShippingInformation)
+  const {
+    saveShippingInformationError,
+    loadingSaveShipping,
+    saveShippingInformationSuccess,
+  } = useSelector((state) => state.saveShippingInformation)
+  const { shippingInformation } = useSelector(
+    (state) => state.fetchShippingInformation
+  )
   const [fullName, setFullName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [email, setEmail] = useState('')
@@ -28,8 +44,17 @@ export default function Shipping() {
   const handleStreetAddressChange = (e) => setStreetAddress(e.target.value)
 
   const onShippingAddressSubmit = (e) => {
-    e.preventDefault();
-    const shippingInformation = {fullName, phoneNumber, email, country, city, province, postCode, streetAddress}
+    e.preventDefault()
+    const shippingInformation = {
+      fullName,
+      phoneNumber,
+      email,
+      country,
+      city,
+      province,
+      postCode,
+      streetAddress,
+    }
     dispatch(saveShippingInformation(shippingInformation))
   }
 
@@ -38,7 +63,7 @@ export default function Shipping() {
   }, [dispatch])
 
   useEffect(() => {
-    if(shippingInformation) {
+    if (shippingInformation) {
       setFullName(shippingInformation.fullName)
       setPhoneNumber(shippingInformation.phoneNumber)
       setEmail(shippingInformation.email)
@@ -52,29 +77,99 @@ export default function Shipping() {
 
   return (
     <View imageUrl={process.env.PUBLIC_URL + shippingImg}>
-    <Heading>Shipping Information</Heading>
-    {loadingSaveShipping && <LoadingSpinner/>}
-    {saveShippingInformationSuccess && <Message top='0%' bottom='0%'>Shipping information saved!</Message>}
-    {fetchShippingInformationError && <Error top='0%' bottom='0%'>{fetchShippingInformationError}</Error>}
-    {saveShippingInformationError && <Error top='0%' bottom='0%'>{saveShippingInformationError}</Error>}
-      <Form onSubmit={onShippingAddressSubmit}>
-        <Input type='text' value={fullName} onChange={handleFullNameChange} />
-        <Input type='text' value={phoneNumber} onChange={handlePhoneNumberChange} />
-        <Input type='email' value={email} onChange={handleEmailChange} />
-        <FormGroup>
-          <Input type='text' value={country} onChange={handleCountryChange} />
-          <Input type='text' value={city} onChange={handleCityChange} />
-        </FormGroup>
-        <FormGroup>
-          <Input type='text' value={province} onChange={handleProvinceChange} />
-          <Input type='text' value={postCode} onChange={handlePostCodeChange} />
-        </FormGroup>
-        <Input type='text' value={streetAddress} onChange={handleStreetAddressChange} />
-        <Button light type='submit'>
-          SAVE
-        </Button>
-      </Form>
+      <Heading>Shipping Information</Heading>
+      {loadingSaveShipping && <LoadingSpinner />}
+      {saveShippingInformationSuccess && (
+        <Message top='0%' bottom='0%'>
+          Shipping information saved!
+        </Message>
+      )}
+      {saveShippingInformationError && (
+        <Error top='0%' bottom='0%'>
+          {saveShippingInformationError}
+        </Error>
+      )}
+      {shippingInformation ? (
+        <Form onSubmit={onShippingAddressSubmit}>
+          <Input type='text' value={fullName} onChange={handleFullNameChange} />
+          <Input
+            type='text'
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+          <Input type='email' value={email} onChange={handleEmailChange} />
+          <FormGroup>
+            <Input type='text' value={country} onChange={handleCountryChange} />
+            <Input type='text' value={city} onChange={handleCityChange} />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              type='text'
+              value={province}
+              onChange={handleProvinceChange}
+            />
+            <Input
+              type='text'
+              value={postCode}
+              onChange={handlePostCodeChange}
+            />
+          </FormGroup>
+          <Input
+            type='text'
+            value={streetAddress}
+            onChange={handleStreetAddressChange}
+          />
+          <Button light type='submit'>
+            SAVE
+          </Button>
+        </Form>
+      ) : (
+        <Form onSubmit={onShippingAddressSubmit}>
+          <Input
+            type='text'
+            placeholder='Full name'
+            onChange={handleFullNameChange}
+          />
+          <Input
+            type='text'
+            placeholder='Phone number'
+            onChange={handlePhoneNumberChange}
+          />
+          <Input
+            type='email'
+            placeholder='Email'
+            onChange={handleEmailChange}
+          />
+          <FormGroup>
+            <Input
+              type='text'
+              placeholder='Country'
+              onChange={handleCountryChange}
+            />
+            <Input type='text' placeholder='City' onChange={handleCityChange} />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              type='text'
+              placeholder='Province'
+              onChange={handleProvinceChange}
+            />
+            <Input
+              type='text'
+              placeholder='Post code'
+              onChange={handlePostCodeChange}
+            />
+          </FormGroup>
+          <Input
+            type='text'
+            placeholder='Street Address'
+            onChange={handleStreetAddressChange}
+          />
+          <Button light type='submit'>
+            SAVE
+          </Button>
+        </Form>
+      )}
     </View>
   )
 }
-

a6fe8526a0fb8b527d0436b2828f4aacf07819a1
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-04 00:52:16 +0100

    can you guess?

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ if (process.env.NODE_ENV === 'production') {
   )
 }
 
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.use(dbMiddleware)
 
 const PORT = process.env.PORT || 5005

58b0bf5fe296f1c1a3f389b1951ce4e507ea4633
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-03 22:44:41 +0100

    edit errors and loading for order details page

--- client/src/views/OrderDetails.js
+++ client/src/views/OrderDetails.js
@@ -1,107 +1,196 @@
-import React, { useEffect} from 'react'
-import { View, SubTitle, List, Row, RowText, Container, HoldImage, SubContainer, Error, LoadingSpinner, Heading } from '../components/MyLibrary'
-import { useDispatch, useSelector } from "react-redux"
-import { useParams} from 'react-router-dom'
-import { fetchOrder } from "../actions/orderActions"
+import React, { useEffect } from 'react'
+import {
+  View,
+  SubTitle,
+  List,
+  Row,
+  RowText,
+  Container,
+  HoldImage,
+  SubContainer,
+  Error,
+  LoadingSpinner,
+  Heading,
+  Button,
+} from '../components/MyLibrary'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { fetchOrder } from '../actions/orderActions'
+import { markOrderDelivered } from '../actions/orderActions'
 import styled from 'styled-components'
 
 export default function OrderDetails() {
-  const {orderId} = useParams()
+  const { orderId } = useParams()
   const dispatch = useDispatch()
-  const { foundOrder, fetchOrderError, fetchOrderLoading } = useSelector((state) => state.fetchOrder)
+  const { foundOrder, fetchOrderError, fetchOrderLoading } = useSelector(
+    (state) => state.fetchOrder
+  )
+  const { markOrderDeliveredSuccess, loadingMarkOrderDelivered } = useSelector(
+    (state) => state.markOrderDelivered
+  )
+  const { userInfo } = useSelector((state) => state.authenticate)
 
   useEffect(() => {
     dispatch(fetchOrder(orderId))
   }, [orderId, dispatch])
 
+  const handleSubmitDelivered = (orderId) => {
+    dispatch(markOrderDelivered(orderId))
+  }
+
+  useEffect(() => {
+    if (markOrderDeliveredSuccess) window.location.reload()
+  }, [markOrderDeliveredSuccess])
+
   return (
     <View responsive>
-    <Heading>Order details</Heading>
-    {fetchOrderLoading && <LoadingSpinner />}
-    {fetchOrderError && <Error>{fetchOrderError}</Error>}
-    {foundOrder && 
-      <Information>
-      <Summary >
-      <SubTitle>Order Summary</SubTitle>
-      <Row>
-        <RowText>Items</RowText>
-        <RowText>${foundOrder.totalPrice}</RowText>
-      </Row>
-      <Row>
-      <RowText>Shipping</RowText>
-        <RowText>$0</RowText>
-      </Row>
-      <Row>
-      <RowText>Tax</RowText>
-        <RowText>${foundOrder.taxPrice}</RowText>
-      </Row>
-      <Row>
-      <RowText>Total</RowText>
-        <RowText>${foundOrder.taxPrice + foundOrder.totalPrice}</RowText>
-      </Row>
-      </Summary>
-      <Shipping>
-      <SubTitle>Shipping</SubTitle>
-        <Row>
-          <RowText>Name</RowText>
-          <RowText>{foundOrder.user.username}</RowText>
-        </Row>
-        <Row>
-          <RowText>Email</RowText>
-          <RowText>{foundOrder.user.email}</RowText>
-        </Row>
-        <Row>
-          <RowText>Address</RowText>
-          <RowText>{foundOrder.shippingAddress.address + ' ' + foundOrder.shippingAddress.city + ' ' + foundOrder.shippingAddress.postCode}</RowText>
-        </Row>
-      </Shipping>
-      <PaymentMethod>
-        <SubTitle>Payment Method</SubTitle>
-        <Row>
-          <RowText>Method Paypal</RowText>
-        </Row>
-      </PaymentMethod>
-      </Information>
-    }
+      <Heading static>Order details</Heading>
+      {loadingMarkOrderDelivered && <LoadingSpinner />}
+      {fetchOrderLoading && <LoadingSpinner />}
+      {fetchOrderError && <Error>{fetchOrderError}</Error>}
+      {foundOrder && (
+        <Information>
+          <Summary>
+            <SubTitle>Order Summary</SubTitle>
+            <Row>
+              <RowText>Items</RowText>
+              <RowText>${foundOrder.totalPrice}</RowText>
+            </Row>
+            <Row>
+              <RowText>Shipping</RowText>
+              <RowText>$0</RowText>
+            </Row>
+            <Row>
+              <RowText>Tax</RowText>
+              <RowText>${foundOrder.taxPrice}</RowText>
+            </Row>
+            <Row>
+              <RowText>Total</RowText>
+              <RowText>${foundOrder.taxPrice + foundOrder.totalPrice}</RowText>
+            </Row>
+          </Summary>
+          <Shipping>
+            <SubTitle>Shipping</SubTitle>
+            <Row>
+              <RowText>Name</RowText>
+              <RowText>{foundOrder.user.username}</RowText>
+            </Row>
+            <Row>
+              <RowText>Email</RowText>
+              <RowText>{foundOrder.user.email}</RowText>
+            </Row>
+            <Row>
+              <RowText>Address</RowText>
+              <RowText>
+                {foundOrder.shippingAddress.address +
+                  ' ' +
+                  foundOrder.shippingAddress.city +
+                  ' ' +
+                  foundOrder.shippingAddress.postCode}
+              </RowText>
+            </Row>
+          </Shipping>
+          <PaymentMethod>
+            <SubTitle>Payment Method</SubTitle>
+            <Row>
+              <RowText>Method</RowText>
+              <RowText>Card</RowText>
+            </Row>
+            <Row>
+              <RowText>Paid</RowText>
+              <RowText>
+                {foundOrder.isPaid
+                  ? foundOrder.paidAt.substring(0, 10)
+                  : 'Not paid'}
+              </RowText>
+            </Row>
+            <Row>
+              <RowText>Delivered</RowText>
+              <RowText>
+                {foundOrder.isDelivered
+                  ? foundOrder.deliveredAt.substring(0, 10)
+                  : 'Not delivered'}
+              </RowText>
+            </Row>
+          </PaymentMethod>
+          {userInfo &&
+            userInfo.isAdmin &&
+            foundOrder.isPaid &&
+            !foundOrder.isDelivered && (
+              <Admin>
+                <SubTitle>Admin</SubTitle>
+                <Row>
+                  <RowText>Mark Delivered</RowText>
+                  <Confirm
+                    onClick={() => handleSubmitDelivered(foundOrder._id)}
+                  >
+                    CONFIRM
+                  </Confirm>
+                </Row>
+              </Admin>
+            )}
+        </Information>
+      )}
       <ItemsList scroll>
-      <SubTitle>Items</SubTitle>
-      {foundOrder && foundOrder.items.map(item => (
-      <Item width='90%' border key={item.itemId}>
-      <HoldImage background>
-        <img src={item.image} alt='' />
-      </HoldImage>
-      <Name flex='3' column center>{item.name}</Name>
-      <Quantity flex='1' column center>{item.quantity}</Quantity>
-      <Price flex='1' column center>${item.price * item.quantity}</Price>
-      </Item>
-      ))}
+        <SubTitle>Items</SubTitle>
+        {foundOrder &&
+          foundOrder.items.map((item) => (
+            <Item width='90%' border key={item.itemId}>
+              <HoldImage background>
+                <img src={item.image} alt='' />
+              </HoldImage>
+              <Name flex='3' column center>
+                {item.name}
+              </Name>
+              <Quantity flex='1' column center>
+                {item.quantity}
+              </Quantity>
+              <Price flex='1' column center>
+                ${item.price * item.quantity}
+              </Price>
+            </Item>
+          ))}
       </ItemsList>
-
     </View>
   )
 }
 
-const Information = styled(List)``
+const Information = styled(List)`
+  margin-bottom: 10rem;
+  padding-bottom: 7rem;
+`
 const Summary = styled(List)``
 const Shipping = styled(List)``
 const PaymentMethod = styled(List)``
-const ItemsList = styled(List)`
-min-height: 100%;
+const Admin = styled(List)`
+  align-items: center;
 `
-const Item = styled(Container)``
+const ItemsList = styled(List)`
+  min-height: 100%;
+`
+const Item = styled(Container)`
+  margin-bottom: 0.5rem;
+  margin-top: 0.5rem;
+`
 
 const Name = styled(SubContainer)`
-font-size: 1.4rem;
-color: rgba(235, 198, 36, 0.945);
-overflow-wrap: break-word;
+  font-size: 1.4rem;
+  color: rgba(235, 198, 36, 0.945);
+  overflow-wrap: break-word;
 `
 const Quantity = styled(SubContainer)`
-color: rgba(235, 198, 36, 0.945);
-font-weight: bold;
-font-size: 1.2rem;
+  color: rgba(235, 198, 36, 0.945);
+  font-weight: bold;
+  font-size: 1.2rem;
 `
 const Price = styled(SubContainer)`
-font-size: 1.4rem;
-color: rgba(235, 198, 36, 0.945);
-overflow-wrap: break-word;
+  font-size: 1.4rem;
+  color: rgba(235, 198, 36, 0.945);
+  overflow-wrap: break-word;
+`
+const Confirm = styled(Button)`
+  width: 10rem;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
 `
